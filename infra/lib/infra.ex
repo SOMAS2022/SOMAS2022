f186084a928a163b1eab247d62bac11cbc425c72
Timothy Moores <info@tmoor.es>
2022-10-21 12:43:16 +0100

    added rpc call & state in infra

--- infra/lib/infra.ex
+++ infra/lib/infra.ex
@@ -2,14 +2,22 @@ defmodule Infra do
   @moduledoc """
   Documentation for `Infra`.
   """
+  use Application
+
+  defstruct [:n_agents, :agents, :dungeon, :time]
 
   @doc """
-    Loop function - handles requests from
+  Loop function - handles requests from
   """
+  @spec loop(state::Infra.struct()) :: no_return()
   def loop(state) do
     receive do
       {:register, process, node} ->
-
+        :io.fwrite("Node: ~p (atomic? ~p), Process; ~p~n", [node, is_atom(node), process])
+        # TODO: Put initial state in here
+        #send(process, {:welcome, :agent})
+        pid = :rpc.call(node, SomasAgent, :init, [])
+        :io.fwrite("Pid is: ~p~n", [pid])
       msg ->
         :io.fwrite("Received ~p~n", [msg])
     end
@@ -19,10 +27,20 @@ defmodule Infra do
   @doc """
     Initial function - registers the infra server globally so it can be called by agents.
   """
-  def init do
+  @spec init(list()) :: no_return()
+  def init(_args\\[]) do
     :global.register_name(:infra_server, self())
     :erlang.register(:infra_server, self())
-    initial_state = %{}
+    initial_state = %Infra{}
     loop(initial_state)
+  end
+
+  def start(_type, args) do
+    # TODO: Resolve args to customise # of agents, etc.
+    {:ok, spawn(fn -> init(args) end)}
+  end
+
+  def stop(_state) do
+    :ok
   end
 end
---
+++ infra/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+mix compile
+iex --name "infra_server@127.0.0.1" --cookie "QKPYDTHMTBVMEDRPLKQD" -S mix
