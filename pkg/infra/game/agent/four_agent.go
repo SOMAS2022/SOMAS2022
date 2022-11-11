b4998bbf511dd6eadd7cb7f44577813714a5c433
Vinayak Unnithan <vinayakunnithan@yahoo.com>
2022-11-11 12:26:17 +0000

    probabilistic agent defined

--- pkg/infra/game/agent/four_agent.go
+++ pkg/infra/game/agent/four_agent.go
@@ -6,17 +6,26 @@ import (
 	"math/rand"
 )
 
-type ThreeAgent struct {
+type FourAgent struct {
 }
 
-func (r ThreeAgent) HandleFight(state state.State, baseAgent BaseAgent) {
+func randFloats(min, max float64, n int) []float64 {
+    res := make([]float64, n)
+    for i := range res {
+        res[i] = min + rand.Float64() * (max - min)
+    }
+    return res
+}
 
-	// Our Agent is brave and always fights!
-	// Hell yeaaah!
-	fight := true
+func (r FourAgent) HandleFight(state state.State, baseAgent BaseAgent) {
+	
+	//We fight almost 8 times out of 10! :)
+	threshold := 0.8
+	fight := randFloats(0,1)
+	
 
 	var action decision.FightAction
-	if fight {
+	if (fight > threshold) {
 		attackVal := rand.Intn(int(state.AgentState[baseAgent.Id].TotalAttack()))
 		defendVal := rand.Intn(int(state.AgentState[baseAgent.Id].TotalDefense()))
 		action = decision.Fight{Attack: uint(attackVal), Defend: uint(defendVal)}
