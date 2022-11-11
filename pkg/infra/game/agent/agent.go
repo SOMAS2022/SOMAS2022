4930fb77c740199ab9da8a5fd59d75981ada994d
neeldug <5161147+neeldug@users.noreply.github.com>
2022-11-11 14:14:24 +0000

    [infra] add previous stage logs (#23)

--- pkg/infra/game/agent/agent.go
+++ pkg/infra/game/agent/agent.go
@@ -1,13 +1,14 @@
 package agent
 
 import (
+	"github.com/benbjohnson/immutable"
 	"infra/game/commons"
 	"infra/game/decision"
 	"infra/game/state"
 )
 
 type Strategy interface {
-	HandleFight(state state.State, baseAgent BaseAgent, decision chan<- decision.FightAction)
+	HandleFight(state state.State, baseAgent BaseAgent, decisionC chan<- decision.FightAction, log *immutable.Map[uint, decision.FightAction])
 }
 
 type Agent struct {
