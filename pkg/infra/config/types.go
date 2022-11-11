c6a983ca2ab3c70b056fb88939d7cf2ff6ea4586
Razvan Rusu <59891885+Razvan00Rusu@users.noreply.github.com>
2022-11-11 14:33:51 +0000

    Added multi-agent type support (in theory)

--- pkg/infra/config/types.go
+++ pkg/infra/config/types.go
@@ -6,7 +6,6 @@ type GameConfig struct {
 	StartingAttackStrength uint
 	StartingShieldStrength uint
 	ThresholdPercentage    float32
-	AgentRandomQty         uint
 	InitialNumAgents       uint
 	Stamina                uint
 }
