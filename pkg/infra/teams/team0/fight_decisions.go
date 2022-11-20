880e1040836c2165544f4943e36e2cb9fe58d900
sherwin-dc <59867245+sherwin-dc@users.noreply.github.com>
2022-11-20 19:25:09 +0000

    Update pkg/infra/teams/team0/fight_decisions.go
    Co-authored-by: neeldug <5161147+neeldug@users.noreply.github.com>

--- pkg/infra/teams/team0/fight_decisions.go
+++ pkg/infra/teams/team0/fight_decisions.go
@@ -5,8 +5,8 @@ import (
 	"infra/game/decision"
 )
 
-func AllDefend(agents map[string]agent.Agent) map[string]decision.FightAction {
-	decisionMap := make(map[string]decision.FightAction)
+func AllDefend(agents map[commons.ID]agent.Agent) map[commons.ID]decision.FightAction {
+	decisionMap := make(map[commons.ID]decision.FightAction)
 
 	for i, _ := range agents {
 		decisionMap[i] = decision.Defend
