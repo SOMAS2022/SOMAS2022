e8ebbf64c5c2b42d58c22f70d09264ca842ed92d
Sherwin da Cruz <59867245+sherwin-dc@users.noreply.github.com>
2022-11-25 16:53:05 +0000

    Add team 1 copyright headers

--- pkg/infra/teams/team1/utils/utils.go
+++ pkg/infra/teams/team1/utils/utils.go
@@ -1,3 +1,12 @@
+/*******************************************************
+* Copyright (C) 2022 Team 1 @ SOMAS2022
+*
+* This file is part of SOMAS2022.
+*
+* This file or its contents can not be copied and/or used
+* without the express permission of Team 1, SOMAS2022
+*******************************************************/
+
 package utils
 
 import (
