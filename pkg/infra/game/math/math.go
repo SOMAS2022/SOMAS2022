da22d88e4020665a9f0f9f55fbd3ce9ec86208d6
Noan Le Renard <43644096+Noanan@users.noreply.github.com>
2022-11-29 12:10:28 +0000

    add tests to math (#77)

--- pkg/infra/game/math/math.go
+++ pkg/infra/game/math/math.go
@@ -6,13 +6,11 @@ import (
 )
 
 func CalculateMonsterHealth(N uint, ST uint, L uint, CL uint) uint {
-	//todo : fix this to be something correct
 	delta := ((float64(rand.Intn(40))) + float64(80)) / float64(100)
 	return uint(math.Ceil((float64(N) * float64(ST) / float64(L)) * delta * (2.0*float64(CL)/float64(L) + float64(0.5))))
 }
 
 func CalculateMonsterDamage(N uint, HP uint, ST uint, TH float32, L uint, CL uint) uint {
-	//todo : fix this to be something correct
 	delta := ((float64(rand.Intn(40))) + float64(80)) / float64(100)
 	M := math.Ceil(float64(N) * float64(TH))
 	N_fp := float64(N)
---
+++ pkg/infra/game/math/math_test.go
@@ -0,0 +1,67 @@
+package game_math
+
+import (
+	"testing"
+)
+
+func TestCalculateMonsterHealth(t *testing.T) {
+	type args struct {
+		N  uint
+		ST uint
+		L  uint
+		CL uint
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantmin uint
+		wantmax uint
+	}{
+		{
+			name: "Case 1",
+			args: args{N: 100, ST: 2000, L: 60, CL: 1},
+			// min for delta = 0.8 and max for delta = 1.2
+			wantmin: 1423,
+			wantmax: 2134,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CalculateMonsterHealth(tt.args.N, tt.args.ST, tt.args.L, tt.args.CL); !(tt.wantmin <= got && got <= tt.wantmax) {
+				t.Errorf("CalculateMonsterHealth() = %v, wanted between %v and %v", got, tt.wantmin, tt.wantmax)
+			}
+		})
+	}
+}
+
+func TestCalculateMonsterDamage(t *testing.T) {
+	type args struct {
+		N  uint
+		HP uint
+		ST uint
+		TH float32
+		L  uint
+		CL uint
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantmin uint
+		wantmax uint
+	}{
+		{
+			name: "Case 1",
+			args: args{N: 100, HP: 1000, ST: 2000, TH: 0.1, L: 60, CL: 1},
+			// min for delta = 0.8 and max for delta = 1.2
+			wantmin: 1922,
+			wantmax: 2848,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CalculateMonsterDamage(tt.args.N, tt.args.HP, tt.args.ST, tt.args.TH, tt.args.L, tt.args.CL); !(tt.wantmin <= got && got <= tt.wantmax) {
+				t.Errorf("CalculateMonsterDamage() = %v,  wanted between %v and %v", got, tt.wantmin, tt.wantmax)
+			}
+		})
+	}
+}
