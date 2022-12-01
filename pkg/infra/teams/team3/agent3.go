6e839cd248d38105bf800b8e91d24d869f2c440e
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2022-12-01 21:56:09 +0000

    Added utility score and comments on functions

--- pkg/infra/teams/team3/agent3.go
+++ pkg/infra/teams/team3/agent3.go
@@ -15,12 +15,31 @@ import (
 )
 
 type AgentThree struct {
-	bravery int
+	bravery      int
+	utilityScore map[commons.ID]int
 }
 
-func (a *AgentThree) FightResolution(baseAgent agent.BaseAgent) tally.Proposal[decision.FightAction] {
+// Update Utility
+func (a *AgentThree) UpdateUtility(baseAgent agent.BaseAgent) {
 
-	baseAgent.Log(logging.Trace, logging.LogField{"bravery": a.bravery, "hp": baseAgent.AgentState().Hp}, "Cowering")
+	view := baseAgent.View()
+	agentState := view.AgentState()
+	itr := agentState.Iterator()
+	for !itr.Done() {
+		id, _, ok := itr.Next()
+		if !ok {
+			break
+		}
+
+		// We are already cool, don't need the utility score for ourselves
+		if id != baseAgent.ID() {
+			a.utilityScore[id] = rand.Intn(10)
+		}
+	}
+}
+
+// Create proposal for fight decisions
+func (a *AgentThree) FightResolution(baseAgent agent.BaseAgent) tally.Proposal[decision.FightAction] {
 
 	actions := make(map[commons.ID]decision.FightAction)
 	view := baseAgent.View()
@@ -31,14 +50,21 @@ func (a *AgentThree) FightResolution(baseAgent agent.BaseAgent) tally.Proposal[d
 		if !ok {
 			break
 		}
-		rNum := rand.Intn(3)
-		switch rNum {
-		case 0:
-			actions[id] = decision.Attack
-		case 1:
-			actions[id] = decision.Defend
-		default:
-			actions[id] = decision.Cower
+
+		// Check for our agent and assign what we want to do
+		if id == baseAgent.ID() {
+			actions[id] = a.CurrentAction()
+			baseAgent.Log(logging.Trace, logging.LogField{"bravery": a.bravery, "hp": baseAgent.AgentState().Hp, "choice": a.CurrentAction()}, "Intent")
+		} else {
+			rNum := rand.Intn(3)
+			switch rNum {
+			case 0:
+				actions[id] = decision.Attack
+			case 1:
+				actions[id] = decision.Defend
+			default:
+				actions[id] = decision.Cower
+			}
 		}
 	}
 	newUUID, _ := uuid.NewUUID()
@@ -46,11 +72,13 @@ func (a *AgentThree) FightResolution(baseAgent agent.BaseAgent) tally.Proposal[d
 	return *prop
 }
 
+// Manifesto
 func (a *AgentThree) CreateManifesto(_ agent.BaseAgent) *decision.Manifesto {
-	manifesto := decision.NewManifesto(true, false, 10, 50)
+	manifesto := decision.NewManifesto(false, false, 10, 50)
 	return manifesto
 }
 
+// Handle No Confidence vote
 func (a *AgentThree) HandleConfidencePoll(_ agent.BaseAgent) decision.Intent {
 	switch rand.Intn(3) {
 	case 0:
@@ -62,8 +90,9 @@ func (a *AgentThree) HandleConfidencePoll(_ agent.BaseAgent) decision.Intent {
 	}
 }
 
+// Send proposal to leader
 func (a *AgentThree) HandleFightInformation(_ message.TaggedMessage, baseAgent agent.BaseAgent, _ *immutable.Map[commons.ID, decision.FightAction]) {
-	baseAgent.Log(logging.Trace, logging.LogField{"bravery": a.bravery, "hp": baseAgent.AgentState().Hp}, "Cowering")
+	// baseAgent.Log(logging.Trace, logging.LogField{"bravery": a.bravery, "hp": baseAgent.AgentState().Hp}, "Cowering")
 	makesProposal := rand.Intn(100)
 
 	if makesProposal > 80 {
@@ -73,19 +102,19 @@ func (a *AgentThree) HandleFightInformation(_ message.TaggedMessage, baseAgent a
 	}
 }
 
-func (a *AgentThree) HandleFightRequest(_ message.TaggedMessage, _ *immutable.Map[commons.ID, decision.FightAction]) message.Payload {
-	return nil
-}
-
+// Calculate our agents action
 func (a *AgentThree) CurrentAction() decision.FightAction {
-	fight := rand.Intn(3)
-	switch fight {
-	case 0:
+
+	if a.bravery > 2 {
+		fight := rand.Intn(2)
+		switch fight {
+		case 0:
+			return decision.Attack
+		default:
+			return decision.Defend
+		}
+	} else {
 		return decision.Cower
-	case 1:
-		return decision.Attack
-	default:
-		return decision.Defend
 	}
 }
 
@@ -117,15 +146,21 @@ func (a *AgentThree) HandleElectionBallot(b agent.BaseAgent, _ *decision.Electio
 	return ballot
 }
 
-func (a *AgentThree) HandleFightProposal(_ *message.FightProposalMessage, _ agent.BaseAgent) decision.Intent {
-	intent := rand.Intn(2)
-	if intent == 0 {
+// Vote on proposal
+func (a *AgentThree) HandleFightProposal(m *message.FightProposalMessage, baseAgent agent.BaseAgent) decision.Intent {
+
+	prop := m.Proposal()
+	act, _ := prop.Get(baseAgent.ID())
+
+	// Selfish, only agree if our decision is ok
+	if act == a.CurrentAction() {
 		return decision.Positive
 	} else {
 		return decision.Negative
 	}
 }
 
+// Leader function to grant the floor
 func (a *AgentThree) HandleFightProposalRequest(_ *message.FightProposalMessage, _ agent.BaseAgent, _ *immutable.Map[commons.ID, decision.FightAction]) bool {
 	switch rand.Intn(2) {
 	case 0:
@@ -135,6 +170,13 @@ func (a *AgentThree) HandleFightProposalRequest(_ *message.FightProposalMessage,
 	}
 }
 
+func (a *AgentThree) HandleFightRequest(_ message.TaggedMessage, _ *immutable.Map[commons.ID, decision.FightAction]) message.Payload {
+	return nil
+}
+
 func NewAgentThree() agent.Strategy {
-	return &AgentThree{bravery: 0}
+	return &AgentThree{
+		bravery:      rand.Intn(10),
+		utilityScore: make(map[string]int),
+	}
 }
