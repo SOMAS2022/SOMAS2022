4fc3daf868822267a6eca11f2f073cd7fc719c98
nd419 <5161147+neeldug@users.noreply.github.com>
2022-12-02 18:28:11 +0000

    Add immutable list

---
+++ pkg/infra/game/commons/immutablelist.go
@@ -0,0 +1,38 @@
+package commons
+
+type ImmutableList[A any] struct {
+	internalList []A
+}
+
+func NewImmutableList[A any](internalList []A) *ImmutableList[A] {
+	return &ImmutableList[A]{internalList: internalList}
+
+}
+
+type Iterator[A any] struct {
+	internal ImmutableList[A]
+	index    int
+}
+
+func NewIterator[A any](internal ImmutableList[A]) *Iterator[A] {
+	return &Iterator[A]{internal: internal}
+}
+
+func (l ImmutableList[A]) Iterator() Iterator[A] {
+	return *NewIterator(l)
+}
+
+func (i *Iterator[A]) Done() bool {
+	return i.index == len(i.internal.internalList)
+}
+
+func (i *Iterator[A]) Next() (A, bool) {
+	defer func() {
+		i.index++
+	}()
+
+	if i.index >= len(i.internal.internalList) {
+		return nil, false
+	}
+	return i.internal.internalList[i.index], true
+}
