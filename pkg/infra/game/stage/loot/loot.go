94214b78a396dd1f60f9d2246eb5c741c0363899
Wenlin Yi <46746329+Weeblin@users.noreply.github.com>
2022-12-02 18:08:15 +0000

    [WIP] add random potion allocation, agent0 left to modify

--- pkg/infra/game/stage/loot/loot.go
+++ pkg/infra/game/stage/loot/loot.go
@@ -79,11 +79,14 @@ func AllocateLoot(globalState state.State, weaponLoot []uint, shieldLoot []uint,
 	allocatedState.PotionSlice.HPpotion = nil
 	allocatedState.PotionSlice.STpotion = nil
 
-	for agentID, agentState := range allocatedState.AgentState {
-		agentState.H = AllocatePotion(allocatedState, agent state.AgentState, HPi int, STi int) state.State
-	}
+	allocatedState.PotionSlice.HPpotion = make([]uint, len(HPpotionloot))
+	allocatedState.PotionSlice.HPpotion = make([]uint, len(STpotionloot))
 
-	
+	for agentID, agentState := range allocatedState.AgentState {
+		allocatedState = AllocateHPPotion(allocatedState, agentID, rand.Intn(len(HPpotionloot)))
+		allocatedState = AllocateSTPotion(allocatedState, agentID, rand.Intn(len(STpotionloot)))
+		allocatedState.AgentState[agentID] = agentState
+	}
 
 	for agentID, agentState := range allocatedState.AgentState {
 		allocatedWeaponIdx := rand.Intn(len(weaponLoot))
