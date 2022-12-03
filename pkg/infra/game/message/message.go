72caa50aac8bde65118219165a6582249202cc5f
Sherwin da Cruz <59867245+sherwin-dc@users.noreply.github.com>
2022-12-03 04:21:11 +0000

    Add message type

--- pkg/infra/game/message/message.go
+++ pkg/infra/game/message/message.go
@@ -73,3 +73,21 @@ func (s StartFight) sealedInform() {
 func (s StartFight) sealedFightInform() {
 	panic("implement me")
 }
+
+type ArrayInfo struct {
+	Num       int
+	StringArr []string
+}
+
+func (s ArrayInfo) sealedMessage() {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (s ArrayInfo) sealedInform() {
+	panic("implement me")
+}
+
+func (s ArrayInfo) sealedFightInform() {
+	panic("implement me")
+}
