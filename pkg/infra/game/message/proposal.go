69275b5c192a60c4242d36c60642958354f16164
nd419 <5161147+neeldug@users.noreply.github.com>
2022-12-04 21:45:36 +0000

    Remove old comment

--- pkg/infra/game/message/proposal.go
+++ pkg/infra/game/message/proposal.go
@@ -25,29 +25,3 @@ func (p Proposal[A]) sealedMessage() {
 func NewProposal[A decision.ProposalAction](proposalID commons.ProposalID, rules commons.ImmutableList[proposal.Rule[A]]) *Proposal[A] {
 	return &Proposal[A]{proposalID: proposalID, rules: rules}
 }
-
-//type MapProposal[A decision.ProposalAction] struct {
-//	proposalID commons.ProposalID
-//	proposal   immutable.Map[commons.ID, A]
-//}
-//
-//func (p MapProposal[A]) sealedProposal() {
-//}
-//
-//func (p MapProposal[A]) sealedMessage() {
-//}
-//
-//func (p MapProposal[A]) ProposalID() commons.ProposalID {
-//	return p.proposalID
-//}
-//
-//func (p MapProposal[A]) Proposal() immutable.Map[commons.ID, A] {
-//	return p.proposal
-//}
-//
-//func NewProposal[A decision.ProposalAction](proposalID commons.ProposalID, proposalMap immutable.Map[commons.ID, A]) *MapProposal[A] {
-//	return &MapProposal[A]{
-//		proposalID: proposalID,
-//		proposal:   proposalMap,
-//	}
-//}
