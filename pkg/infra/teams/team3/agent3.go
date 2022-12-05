caff6e4d73267fc0aed52248ff897b8b26e9be36
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2022-12-05 04:37:55 +0000

    Modified internal state, added struct for sorting utility

--- pkg/infra/teams/team3/agent3.go
+++ pkg/infra/teams/team3/agent3.go
@@ -13,7 +13,23 @@ import (
 	"github.com/google/uuid"
 )
 
+const PERCENTAGE = 500
+
+type Utility struct {
+	ID    commons.ID
+	score int
+}
+
+type UtilityMap []Utility
+
+func (u UtilityMap) Len() int           { return len(u) }
+func (u UtilityMap) Swap(i, j int)      { u[i], u[j] = u[j], u[i] }
+func (u UtilityMap) Less(i, j int) bool { return u[i].score < u[j].score }
+
 type AgentThree struct {
+	HP           int
+	ST           int
+	AT           int
 	bravery      int
 	utilityScore map[commons.ID]int
 }
@@ -21,9 +37,9 @@ type AgentThree struct {
 // HP pool donation
 func (a *AgentThree) DonateToHpPool(baseAgent agent.BaseAgent) uint {
 	donation := rand.Intn(2)
-	// If our health is > 50% then donate some HP
-	if donation == 1 && baseAgent.AgentState().Hp > 500 {
-		return uint(rand.Intn(int((baseAgent.AgentState().Hp * 20) / 100)))
+	// If our health is > 50% and we feel generous then donate some (max 20%) HP
+	if donation == 1 && a.HP > PERCENTAGE {
+		return uint(rand.Intn((a.HP * 20) / 100))
 	} else {
 		return 0
 	}
@@ -31,6 +47,9 @@ func (a *AgentThree) DonateToHpPool(baseAgent agent.BaseAgent) uint {
 
 func (a *AgentThree) UpdateInternalState(baseAgent agent.BaseAgent, _ *commons.ImmutableList[decision.ImmutableFightResult], _ *immutable.Map[decision.Intent, uint]) {
 	a.UpdateUtility(baseAgent)
+	a.HP = int(baseAgent.AgentState().Hp)
+	a.ST = int(baseAgent.AgentState().Stamina)
+	a.AT = int(baseAgent.AgentState().Attack)
 }
 
 // Update Utility
@@ -49,6 +68,8 @@ func (a *AgentThree) UpdateUtility(baseAgent agent.BaseAgent) {
 			a.utilityScore[id] = rand.Intn(10)
 		}
 	}
+	// Sort utility map
+	// sort.Sort(a.utilityScore)
 }
 
 // Create proposal for fight decisions
@@ -66,8 +87,14 @@ func (a *AgentThree) FightResolution(baseAgent agent.BaseAgent) message.MapPropo
 		// Check for our agent and assign what we want to do
 		if id == baseAgent.ID() {
 			actions[id] = a.CurrentAction()
-			baseAgent.Log(logging.Trace, logging.LogField{"bravery": a.bravery, "hp": baseAgent.AgentState().Hp, "choice": a.CurrentAction()}, "Intent")
+			baseAgent.Log(logging.Trace, logging.LogField{"bravery": a.bravery, "hp": a.HP, "choice": a.CurrentAction(), "util": a.utilityScore[view.CurrentLeader()]}, "Intent")
 		} else {
+			// Send some messages to other agents
+			// send := rand.Intn(5)
+			// if send == 0 {
+			// 	m := message.FightInform()
+			// 	_ = baseAgent.SendBlockingMessage(id, m)
+			// }
 			rNum := rand.Intn(3)
 			switch rNum {
 			case 0:
@@ -91,14 +118,18 @@ func (a *AgentThree) CreateManifesto(_ agent.BaseAgent) *decision.Manifesto {
 }
 
 // Handle No Confidence vote
-func (a *AgentThree) HandleConfidencePoll(_ agent.BaseAgent) decision.Intent {
-	switch rand.Intn(3) {
-	case 0:
-		return decision.Abstain
-	case 1:
-		return decision.Negative
-	default:
+func (a *AgentThree) HandleConfidencePoll(baseAgent agent.BaseAgent) decision.Intent {
+	view := baseAgent.View()
+	// Vote for leader to stay if he's our friend :)
+	if a.utilityScore[view.CurrentLeader()] > 5 {
 		return decision.Positive
+	} else {
+		switch rand.Intn(2) {
+		case 0:
+			return decision.Abstain
+		default:
+			return decision.Negative
+		}
 	}
 }
 
@@ -107,6 +138,7 @@ func (a *AgentThree) HandleFightInformation(_ message.TaggedInformMessage[messag
 	// baseAgent.Log(logging.Trace, logging.LogField{"bravery": r.bravery, "hp": baseAgent.AgentState().Hp}, "Cowering")
 	makesProposal := rand.Intn(100)
 
+	// Well, not everytime. Just sometimes
 	if makesProposal > 80 {
 		prop := a.FightResolution(baseAgent)
 		_ = baseAgent.SendProposalToLeader(prop)
@@ -115,7 +147,8 @@ func (a *AgentThree) HandleFightInformation(_ message.TaggedInformMessage[messag
 
 // Calculate our agents action
 func (a *AgentThree) CurrentAction() decision.FightAction {
-	if a.bravery > 2 {
+	// Always check how bravee we are and if we have the Stamina to do it...
+	if a.bravery > 3 && a.ST > PERCENTAGE {
 		fight := rand.Intn(2)
 		switch fight {
 		case 0:
@@ -128,9 +161,9 @@ func (a *AgentThree) CurrentAction() decision.FightAction {
 	}
 }
 
-func (a *AgentThree) HandleElectionBallot(b agent.BaseAgent, _ *decision.ElectionParams) decision.Ballot {
+func (a *AgentThree) HandleElectionBallot(baseAgent agent.BaseAgent, _ *decision.ElectionParams) decision.Ballot {
 	// Extract ID of alive agents
-	view := b.View()
+	view := baseAgent.View()
 	agentState := view.AgentState()
 	aliveAgentIDs := make([]string, agentState.Len())
 	i := 0
@@ -169,7 +202,7 @@ func (a *AgentThree) HandleFightProposal(m message.FightProposalMessage, baseAge
 	}
 }
 
-func (a *AgentThree) HandleUpdateWeapon(b agent.BaseAgent) decision.ItemIdx {
+func (a *AgentThree) HandleUpdateWeapon(baseAgent agent.BaseAgent) decision.ItemIdx {
 	// weapons := b.AgentState().Weapons
 	// return decision.ItemIdx(rand.Intn(weapons.Len() + 1))
 
@@ -177,7 +210,7 @@ func (a *AgentThree) HandleUpdateWeapon(b agent.BaseAgent) decision.ItemIdx {
 	return decision.ItemIdx(0)
 }
 
-func (a *AgentThree) HandleUpdateShield(b agent.BaseAgent) decision.ItemIdx {
+func (a *AgentThree) HandleUpdateShield(baseAgent agent.BaseAgent) decision.ItemIdx {
 	// shields := b.AgentState().Shields
 	// return decision.ItemIdx(rand.Intn(shields.Len() + 1))
 	return decision.ItemIdx(0)
