7cf43fb24d665ae8d7bf3c45511ae3f98a05fb0f
Wenlin Yi <46746329+Weeblin@users.noreply.github.com>
2022-12-06 20:25:13 +0000

    [Agent] Build Agent Infra (#125)
    * WIP update agent infra+allocation+leaderfight
    
    * Agent5 flow built (with essential functions only)

---
+++ pkg/infra/team5/Pkg5/leaderFight/leaderFight.go
@@ -0,0 +1,12 @@
+package leaderFight
+
+import (
+	"infra/game/message"
+	"infra/game/state"
+)
+
+func LeaderFightMessageHandler(s *state.View) *message.TaggedMessage {
+	var leaderFightMessage *message.TaggedMessage
+
+	return leaderFightMessage
+}
