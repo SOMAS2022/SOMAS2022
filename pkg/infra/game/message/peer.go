addb139acc5d5048d10e9e291ff1798817b4fd2f
Sherwin da Cruz <59867245+sherwin-dc@users.noreply.github.com>
2022-12-06 16:09:12 +0000

    Make peer message more robust

--- pkg/infra/game/message/peer.go
+++ pkg/infra/game/message/peer.go
@@ -1,8 +1,35 @@
 package message
 
+import (
+	"github.com/benbjohnson/immutable"
+)
+
 type ArrayInfo struct {
-	Num       int
-	StringArr []string
+	num       int
+	stringArr immutable.List[string]
+}
+
+func NewArrayInfo(num int, strArr []string) ArrayInfo {
+	builder := immutable.NewListBuilder[string]()
+	for i, str := range strArr {
+		builder.Set(i, str)
+	}
+
+	return ArrayInfo{num: num, stringArr: *builder.List()}
+}
+
+func (s ArrayInfo) GetStringArr() []string {
+	strArr := make([]string, 0, s.stringArr.Len())
+	it := s.stringArr.Iterator()
+	for !it.Done() {
+		_, str := it.Next()
+		strArr = append(strArr, str)
+	}
+	return strArr
+}
+
+func (s ArrayInfo) GetNum() int {
+	return s.num
 }
 
 func (s ArrayInfo) sealedMessage() {
