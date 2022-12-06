5e7a16e9c3a2d59badfda2ee10c86834dedab723
Wenlin Yi <46746329+Weeblin@users.noreply.github.com>
2022-12-06 20:25:13 +0000

    [Agent] Build Agent Infra (#125)
    * WIP update agent infra+allocation+leaderfight
    
    * Agent5 flow built (with essential functions only)

--- pkg/infra/team5/agent5.go
+++ pkg/infra/team5/agent5.go
@@ -1,1 +1,22 @@
 package team5
+
+import (
+	"infra/game/message"
+	"infra/game/state"
+	"infra/team5/Pkg5/allocation"
+	"infra/team5/Pkg5/leaderFight"
+)
+
+type ReturnType struct {
+	allocationMessage  *message.TaggedMessage
+	leaderFightMessage *message.TaggedMessage
+}
+
+func team5(allocM *message.TaggedMessage, s *state.View) ReturnType {
+	var returnType ReturnType
+
+	returnType.allocationMessage = allocation.AllocMessageHandler(allocM, s)
+	returnType.leaderFightMessage = leaderFight.LeaderFightMessageHandler(s)
+
+	return returnType
+}
