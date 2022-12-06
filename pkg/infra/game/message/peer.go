ae774a0845707d5c42fc88c461f12522c0a1ee45
Sherwin da Cruz <59867245+sherwin-dc@users.noreply.github.com>
2022-12-06 15:20:24 +0000

    Add new message format

---
+++ pkg/infra/game/message/peer.go
@@ -0,0 +1,19 @@
+package message
+
+type ArrayInfo struct {
+	Num       int
+	StringArr []string
+}
+
+func (s ArrayInfo) sealedMessage() {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (s ArrayInfo) sealedInform() {
+	panic("implement me")
+}
+
+func (s ArrayInfo) sealedFightInform() {
+	panic("implement me")
+}
