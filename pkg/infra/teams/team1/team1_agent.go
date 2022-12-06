fe646a60bb939e4b9e04b5f773dd735d01994852
Sherwin da Cruz <59867245+sherwin-dc@users.noreply.github.com>
2022-12-06 14:28:00 +0000

    Rebase on main

--- pkg/infra/teams/team1/team1_agent.go
+++ pkg/infra/teams/team1/team1_agent.go
@@ -14,11 +14,12 @@ import (
 	"infra/game/commons"
 	"infra/game/decision"
 	"infra/game/message"
+	"infra/game/message/proposal"
+	"infra/game/state"
 	"infra/teams/team1/utils"
 	"math/rand"
 
 	"github.com/benbjohnson/immutable"
-	"github.com/google/uuid"
 )
 
 type SocialAgent struct {
@@ -90,29 +91,26 @@ func (r *SocialAgent) UpdateInternalState(agent agent.BaseAgent, _ *commons.Immu
 }
 
 // TODO: Currently default
-func (r *SocialAgent) FightResolution(baseAgent agent.BaseAgent) message.MapProposal[decision.FightAction] {
-	actions := make(map[commons.ID]decision.FightAction)
-	view := baseAgent.View()
-	agentState := view.AgentState()
-	itr := agentState.Iterator()
-	for !itr.Done() {
-		id, _, ok := itr.Next()
-		if !ok {
-			break
-		}
+func (r *SocialAgent) FightResolution(_ agent.BaseAgent) commons.ImmutableList[proposal.Rule[decision.FightAction]] {
+	rules := make([]proposal.Rule[decision.FightAction], 0)
 
-		switch rand.Intn(3) {
-		case 0:
-			actions[id] = decision.Attack
-		case 1:
-			actions[id] = decision.Defend
-		default:
-			actions[id] = decision.Cower
-		}
-	}
-	newUUID, _ := uuid.NewUUID()
-	prop := message.NewProposal(newUUID.String(), commons.MapToImmutable(actions))
-	return *prop
+	rules = append(rules, *proposal.NewRule[decision.FightAction](decision.Attack,
+		proposal.NewComparativeCondition(proposal.Health, proposal.GreaterThan, 1000),
+	))
+
+	rules = append(rules, *proposal.NewRule[decision.FightAction](decision.Defend,
+		proposal.NewComparativeCondition(proposal.TotalDefence, proposal.GreaterThan, 1000),
+	))
+
+	rules = append(rules, *proposal.NewRule[decision.FightAction](decision.Cower,
+		proposal.NewComparativeCondition(proposal.Health, proposal.LessThan, 1),
+	))
+
+	rules = append(rules, *proposal.NewRule[decision.FightAction](decision.Attack,
+		proposal.NewComparativeCondition(proposal.Stamina, proposal.GreaterThan, 10),
+	))
+
+	return *commons.NewImmutableList(rules)
 }
 
 // TODO: Currently default
@@ -183,7 +181,7 @@ func (r *SocialAgent) HandleFightRequest(m message.TaggedRequestMessage[message.
 	return nil
 }
 
-func (r *SocialAgent) CurrentAction() decision.FightAction {
+func (r *SocialAgent) FightAction() decision.FightAction {
 	return r.currentDecision
 }
 
@@ -217,7 +215,7 @@ func (r *SocialAgent) HandleElectionBallot(b agent.BaseAgent, _ *decision.Electi
 }
 
 // TODO: Currently default
-func (r *SocialAgent) HandleFightProposal(_ message.FightProposalMessage, _ agent.BaseAgent) decision.Intent {
+func (r *SocialAgent) HandleFightProposal(_ message.Proposal[decision.FightAction], _ agent.BaseAgent) decision.Intent {
 	intent := rand.Intn(2)
 	if intent == 0 {
 		return decision.Positive
@@ -227,7 +225,7 @@ func (r *SocialAgent) HandleFightProposal(_ message.FightProposalMessage, _ agen
 }
 
 // TODO: Currently default
-func (r *SocialAgent) HandleFightProposalRequest(_ message.FightProposalMessage, _ agent.BaseAgent, _ *immutable.Map[commons.ID, decision.FightAction]) bool {
+func (r *SocialAgent) HandleFightProposalRequest(_ message.Proposal[decision.FightAction], _ agent.BaseAgent, _ *immutable.Map[commons.ID, decision.FightAction]) bool {
 	switch rand.Intn(2) {
 	case 0:
 		return true
@@ -250,4 +248,72 @@ func (r *SocialAgent) HandleUpdateShield(_ agent.BaseAgent) decision.ItemIdx {
 	// shields := b.AgentState().Shields
 	// return decision.ItemIdx(rand.Intn(shields.Len() + 1))
 	return decision.ItemIdx(0)
+}
+
+// TODO: Currently default
+func (r *SocialAgent) HandleLootInformation(m message.TaggedInformMessage[message.LootInform], agent agent.BaseAgent) {
+}
+
+// TODO: Currently default
+func (r *SocialAgent) HandleLootRequest(m message.TaggedRequestMessage[message.LootRequest]) message.LootInform {
+	//TODO implement me
+	panic("implement me")
+}
+
+// TODO: Currently default
+func (r *SocialAgent) HandleLootProposal(_ message.Proposal[decision.LootAction], _ agent.BaseAgent) decision.Intent {
+	switch rand.Intn(3) {
+	case 0:
+		return decision.Positive
+	case 1:
+		return decision.Negative
+	default:
+		return decision.Abstain
+	}
+}
+
+// TODO: Currently default
+func (r *SocialAgent) HandleLootProposalRequest(_ message.Proposal[decision.LootAction], _ agent.BaseAgent) bool {
+	switch rand.Intn(2) {
+	case 0:
+		return true
+	default:
+		return false
+	}
+}
+
+// TODO: Currently default
+func (r *SocialAgent) LootAllocation(ba agent.BaseAgent) immutable.Map[commons.ID, immutable.List[commons.ItemID]] {
+	lootAllocation := make(map[commons.ID][]commons.ItemID)
+	view := ba.View()
+	ids := commons.ImmutableMapKeys(view.AgentState())
+	iterator := ba.Loot().Weapons().Iterator()
+	allocateRandomly(iterator, ids, lootAllocation)
+	iterator = ba.Loot().Shields().Iterator()
+	allocateRandomly(iterator, ids, lootAllocation)
+	iterator = ba.Loot().HpPotions().Iterator()
+	allocateRandomly(iterator, ids, lootAllocation)
+	iterator = ba.Loot().StaminaPotions().Iterator()
+	allocateRandomly(iterator, ids, lootAllocation)
+	mMapped := make(map[commons.ID]immutable.List[commons.ItemID])
+	for id, itemIDS := range lootAllocation {
+		mMapped[id] = commons.ListToImmutable(itemIDS)
+	}
+	return commons.MapToImmutable(mMapped)
+}
+
+// TODO: Currently default
+func allocateRandomly(iterator commons.Iterator[state.Item], ids []commons.ID, lootAllocation map[commons.ID][]commons.ItemID) {
+	for !iterator.Done() {
+		next, _ := iterator.Next()
+		toBeAllocated := ids[rand.Intn(len(ids))]
+		if l, ok := lootAllocation[toBeAllocated]; ok {
+			l = append(l, next.Id())
+			lootAllocation[toBeAllocated] = l
+		} else {
+			l := make([]commons.ItemID, 0)
+			l = append(l, next.Id())
+			lootAllocation[toBeAllocated] = l
+		}
+	}
 }
