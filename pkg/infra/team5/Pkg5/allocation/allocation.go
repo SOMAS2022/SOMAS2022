3f61138b6c9c44c47ead70f68ca565f3408d4fe1
Wenlin Yi <2074270068@qq.com>
2022-12-06 19:45:47 +0000

    WIP update agent infra+allocation+leaderfight

--- pkg/infra/team5/Pkg5/allocation/allocation.go
+++ pkg/infra/team5/Pkg5/allocation/allocation.go
@@ -2,10 +2,9 @@ package allocation
 
 import (
 	"infra/game/commons"
+	"infra/game/message"
 	"infra/game/state"
 )
-
-func AllocMessageHandler() {}
 
 type ClashItem struct {
 	ItemID          commons.ID
@@ -15,11 +14,52 @@ type ClashItem struct {
 	winner          commons.ID
 }
 
-// may not be necessary depending on infra
-func FindClashLoot(s *state.View) []ClashItem {
-	var ClashLoot []ClashItem
+// handles incomming message, whether
+func AllocMessageHandler(m *message.TaggedMessage, s *state.View) *message.TaggedMessage {
+	var clashSet []ClashItem
+	var winnerSet []ClashItem
+	//function to be decided depending on the messaging method;
+	//-----------------------------//
 
+	//-----------------------------//
+	//AllocMessageHandle handles all allocation message and return allocation decisions.
+	//-----------------------------//
+	clashSet = FindClashLoot()
+	winnerSet = FindWinnerSet(s, clashSet)
+	//-----------------------------//
+	AllocationMessage := clashLoot2Message(winnerSet)
+	return AllocationMessage
+}
+
+// may not be necessary depending on infra
+func message2ClashLoot(m *message.TaggedMessage) []ClashItem {
+	var ClashLoot []ClashItem
+	//from message to ClashLoot
+	ClashLoot = FindClashLoot()
 	return ClashLoot
+}
+func clashLoot2Message(cl []ClashItem) *message.TaggedMessage {
+	var m *message.TaggedMessage
+	//convert struct to message
+	return m
+}
+
+func FindClashLoot() []ClashItem {
+	var ClashLoot []ClashItem
+	//build clashloot struct from all information/messages
+	return ClashLoot
+}
+
+func FindWinner(s *state.View, cl ClashItem) ClashItem {
+	var Winner ClashItem
+	//function fot resolving clash one by one
+	return Winner
+}
+
+func FindWinnerSet(s *state.View, cl []ClashItem) []ClashItem {
+	var Winner []ClashItem
+	//function fot resolving all clashes at the same time
+	return Winner
 }
 
 type TotalResource struct{}
--- pkg/infra/team5/agent5.go
+++ pkg/infra/team5/agent5.go
@@ -1,1 +1,18 @@
 package team5
+
+import (
+	"infra/game/state"
+	"infra/team5/Pkg5/allocation"
+	"infra/team5/Pkg5/commons5"
+)
+
+type returnType struct {
+	allocationMessage
+}
+
+func team5(s *state.View) {
+	var clashSet []allocation.ClashItem
+	var winnerSet []allocation.ClashItem
+	agentstates := commons5.FetchAllAgents(s)
+	winnerSet = allocation.AllocMessageHandler(agentstates, clashSet)
+}
