cfd5411dab7e5024ad69857f6ee3bfe317e2f0b2
Wenlin Yi <46746329+Weeblin@users.noreply.github.com>
2022-12-06 20:25:13 +0000

    [Agent] Build Agent Infra (#125)
    * WIP update agent infra+allocation+leaderfight
    
    * Agent5 flow built (with essential functions only)

--- pkg/infra/team5/Pkg5/allocation/allocation.go
+++ pkg/infra/team5/Pkg5/allocation/allocation.go
@@ -2,10 +2,9 @@ package allocation
 
 import (
 	"infra/game/commons"
+	"infra/game/message"
 	"infra/game/state"
 )
-
-func AllocMessageHandler() {}
 
 type ClashItem struct {
 	ItemID          commons.ID
@@ -15,11 +14,50 @@ type ClashItem struct {
 	winner          commons.ID
 }
 
-// may not be necessary depending on infra
-func FindClashLoot(s *state.View) []ClashItem {
-	var ClashLoot []ClashItem
+// handles incomming message, whether
+func AllocMessageHandler(m *message.TaggedMessage, s *state.View) *message.TaggedMessage {
+	clashSet := message2ClashLoot(m)
+	//function to be decided depending on the messaging method;
+	//-----------------------------//
 
+	//-----------------------------//
+	//AllocMessageHandle handles all allocation message and return allocation decisions.
+	//-----------------------------//
+	winnerSet := FindWinnerSet(s, clashSet)
+	//-----------------------------//
+	AllocationMessage := clashLoot2Message(winnerSet)
+	return AllocationMessage
+}
+
+// may not be necessary depending on infra
+func message2ClashLoot(m *message.TaggedMessage) []ClashItem {
+	var ClashLoot []ClashItem
+	//from message to ClashLoot
+	ClashLoot = FindClashLoot()
 	return ClashLoot
+}
+func clashLoot2Message(cl []ClashItem) *message.TaggedMessage {
+	var m *message.TaggedMessage
+	//convert struct to message
+	return m
+}
+
+func FindClashLoot() []ClashItem {
+	var ClashLoot []ClashItem
+	//build clashloot struct from all information/messages
+	return ClashLoot
+}
+
+func FindWinner(s *state.View, cl ClashItem) ClashItem {
+	var Winner ClashItem
+	//function fot resolving clash one by one
+	return Winner
+}
+
+func FindWinnerSet(s *state.View, cl []ClashItem) []ClashItem {
+	var Winner []ClashItem
+	//function fot resolving all clashes at the same time
+	return Winner
 }
 
 type TotalResource struct{}
