8b50c4250d50c2b88b6e5725a23e33a5960eefac
Wenlin Yi <46746329+Weeblin@users.noreply.github.com>
2022-12-06 12:31:26 +0000

    [Agent][WIP]Add agent main+commons functions structures+alloc peseudo (#113)
    * Add agent main+commons functions+alloc peseudo
    
    * format linting
    
    * more updates
    
    * fix typo

---
+++ pkg/infra/team5/Pkg5/allocation/allocation.go
@@ -0,0 +1,27 @@
+package allocation
+
+import (
+	"infra/game/commons"
+	"infra/game/state"
+)
+
+func AllocMessageHandler() {}
+
+type ClashItem struct {
+	ItemID          commons.ID
+	Type            string
+	Value           uint
+	RequestedAgents []commons.ID
+	winner          commons.ID
+}
+
+// may not be necessary depending on infra
+func FindClashLoot(s *state.View) []ClashItem {
+	var ClashLoot []ClashItem
+
+	return ClashLoot
+}
+
+type TotalResource struct{}
+
+func OptimalAlloc(totalAlives uint, totalResource TotalResource) {}
