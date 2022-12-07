d4bc601f26b64abccf919fec516c74971c63078e
Wenlin Yi <46746329+Weeblin@users.noreply.github.com>
2022-12-07 13:49:39 +0000

    update file locations

--- pkg/infra/teams/team5/agent5.go
+++ pkg/infra/teams/team5/agent5.go
@@ -1,6 +1,7 @@
-package agent
+package team5
 
 import (
+	"infra/game/agent"
 	"infra/game/commons"
 	"infra/game/decision"
 	"infra/game/message"
@@ -16,36 +17,36 @@ type agent5 struct {
 	InternalState   internalState
 }
 
-func (t *agent5) CreateManifesto(baseAgent BaseAgent) *decision.Manifesto {
+func (t *agent5) CreateManifesto(baseAgent agent.BaseAgent) *decision.Manifesto {
 	var returnType *decision.Manifesto
 	return returnType
 }
-func (t *agent5) HandleConfidencePoll(baseAgent BaseAgent) decision.Intent {
+func (t *agent5) HandleConfidencePoll(baseAgent agent.BaseAgent) decision.Intent {
 	var returnType decision.Intent
 	return returnType
 }
-func (t *agent5) HandleElectionBallot(baseAgent BaseAgent, params *decision.ElectionParams) decision.Ballot {
+func (t *agent5) HandleElectionBallot(baseAgent agent.BaseAgent, params *decision.ElectionParams) decision.Ballot {
 	var returnType decision.Ballot
 	return returnType
 }
 
-func (t *agent5) HandleFightInformation(m message.TaggedInformMessage[message.FightInform], baseAgent BaseAgent, log *immutable.Map[commons.ID, decision.FightAction]) {
+func (t *agent5) HandleFightInformation(m message.TaggedInformMessage[message.FightInform], ba agent.BaseAgent, log *immutable.Map[commons.ID, decision.FightAction]) {
 }
 func (t *agent5) HandleFightRequest(m message.TaggedRequestMessage[message.FightRequest], log *immutable.Map[commons.ID, decision.FightAction]) message.FightInform {
 	var returnType message.FightInform
 	return returnType
 }
-func (t *agent5) FightResolution(agent BaseAgent) commons.ImmutableList[proposal.Rule[decision.FightAction]] {
+func (t *agent5) FightResolution(agent agent.BaseAgent) commons.ImmutableList[proposal.Rule[decision.FightAction]] {
 	var returnType commons.ImmutableList[proposal.Rule[decision.FightAction]]
 	return returnType
 }
-func (t *agent5) HandleFightProposal(proposal message.Proposal[decision.FightAction], baseAgent BaseAgent) decision.Intent {
+func (t *agent5) HandleFightProposal(proposal message.Proposal[decision.FightAction], ba agent.BaseAgent) decision.Intent {
 	var returnType decision.Intent
 	return returnType
 }
 
 // HandleFightProposalRequest only called as leader
-func (t *agent5) HandleFightProposalRequest(proposal message.Proposal[decision.FightAction], baseAgent BaseAgent, log *immutable.Map[commons.ID, decision.FightAction]) bool {
+func (t *agent5) HandleFightProposalRequest(proposal message.Proposal[decision.FightAction], ba agent.BaseAgent, log *immutable.Map[commons.ID, decision.FightAction]) bool {
 	var returnType bool
 	return returnType
 }
@@ -57,41 +58,41 @@ func (t *agent5) HandleFightProposalRequest(proposal message.Proposal[decision.F
 // 	return returnType
 // }
 
-func (t *agent5) HandleLootInformation(m message.TaggedInformMessage[message.LootInform], agent BaseAgent) {
+func (t *agent5) HandleLootInformation(m message.TaggedInformMessage[message.LootInform], agent agent.BaseAgent) {
 }
 func (t *agent5) HandleLootRequest(m message.TaggedRequestMessage[message.LootRequest]) message.LootInform {
 	var returnType message.LootInform
 	return returnType
 }
-func (t *agent5) HandleLootProposal(r message.Proposal[decision.LootAction], agent BaseAgent) decision.Intent {
+func (t *agent5) HandleLootProposal(r message.Proposal[decision.LootAction], agent agent.BaseAgent) decision.Intent {
 	var rr decision.Intent
 	return rr
 }
-func (t *agent5) HandleLootProposalRequest(proposal message.Proposal[decision.LootAction], agent BaseAgent) bool {
+func (t *agent5) HandleLootProposalRequest(proposal message.Proposal[decision.LootAction], agent agent.BaseAgent) bool {
 	var returnType bool
 	return returnType
 }
-func (t *agent5) LootAllocation(agent BaseAgent) immutable.Map[commons.ID, immutable.List[commons.ItemID]] {
+func (t *agent5) LootAllocation(agent agent.BaseAgent) immutable.Map[commons.ID, immutable.List[commons.ItemID]] {
 	var returnType immutable.Map[commons.ID, immutable.List[commons.ItemID]]
 	return returnType
 }
 
 // HandleUpdateWeapon return the index of the weapon you want to use in AgentState.weapons
-func (t *agent5) HandleUpdateWeapon(baseAgent BaseAgent) decision.ItemIdx {
+func (t *agent5) HandleUpdateWeapon(baseAgent agent.BaseAgent) decision.ItemIdx {
 	var returnType decision.ItemIdx
 	return returnType
 }
 
 // HandleUpdateShield return the index of the shield you want to use in AgentState.Shields
-func (t *agent5) HandleUpdateShield(baseAgent BaseAgent) decision.ItemIdx {
+func (t *agent5) HandleUpdateShield(baseAgent agent.BaseAgent) decision.ItemIdx {
 	var returnType decision.ItemIdx
 	return returnType
 }
 
-func (t *agent5) UpdateInternalState(baseAgent BaseAgent, fightResult *commons.ImmutableList[decision.ImmutableFightResult], voteResult *immutable.Map[decision.Intent, uint]) {
+func (t *agent5) UpdateInternalState(baseAgent agent.BaseAgent, fightResult *commons.ImmutableList[decision.ImmutableFightResult], voteResult *immutable.Map[decision.Intent, uint]) {
 }
 
-func (t *agent5) DonateToHpPool(baseAgent BaseAgent) uint {
+func (t *agent5) DonateToHpPool(baseAgent agent.BaseAgent) uint {
 	var returnType uint
 	return returnType
 }
@@ -101,6 +102,6 @@ type internalState struct {
 	//......
 }
 
-func NewAgen5() Strategy {
+func NewAgen5() agent.Strategy {
 	return &agent5{}
 }
