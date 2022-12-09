7c7f484eadd521149a18ab367e8d52df07f12c32
Sherwin da Cruz <59867245+sherwin-dc@users.noreply.github.com>
2022-12-09 12:01:12 +0000

    Update update function

--- pkg/infra/teams/team1/init.go
+++ pkg/infra/teams/team1/init.go
@@ -62,18 +62,30 @@ func InitAgents(defaultStrategyMap map[commons.ID]func() agent.Strategy, gameCon
 	return
 }
 
-func UpdateInternalStates(agentMap map[commons.ID]agent.Agent, globalState *state.State, immutableFightRounds *commons.ImmutableList[decision.ImmutableFightResult], votesResult *immutable.Map[decision.Intent, uint], logChan chan<- logging.AgentLog) {
+func UpdateInternalStates(agentMap map[commons.ID]agent.Agent, globalState *state.State, immutableFightRounds *commons.ImmutableList[decision.ImmutableFightResult], votesResult *immutable.Map[decision.Intent, uint]) map[commons.ID]logging.AgentLog {
 	var wg sync.WaitGroup
+	agentLogChan := make(chan logging.AgentLog)
 	for id, a := range agentMap {
 		id := id
 		a := a
 		wg.Add(1)
 		go func(wait *sync.WaitGroup) {
-			a.HandleUpdateInternalState(globalState.AgentState[id], immutableFightRounds, votesResult, logChan)
+			a.HandleUpdateInternalState(globalState.AgentState[id], immutableFightRounds, votesResult, agentLogChan)
 			wait.Done()
 		}(&wg)
 	}
+
+	agentLogs := make(map[commons.ID]logging.AgentLog)
+	go func(agentLogChan chan logging.AgentLog, agentLogs map[commons.ID]logging.AgentLog) {
+		for log := range agentLogChan {
+			agentLogs[log.ID] = log
+		}
+	}(agentLogChan, agentLogs)
 	wg.Wait()
+	// fmt.Println(agentLogs)
+	close(agentLogChan)
+	// fmt.Println(agentLogs)
 
 	printGraph(agentMap, globalState)
+	return agentLogs
 }
