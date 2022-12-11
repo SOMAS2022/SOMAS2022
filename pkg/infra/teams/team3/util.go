0b7b9937bad60b6fc5cc9ae73a51a4c2b6cb6e1d
SachaHakim20 <98891615+SachaHakim20@users.noreply.github.com>
2022-12-11 18:02:36 +0000

    utility pseudocodes

--- pkg/infra/teams/team3/util.go
+++ pkg/infra/teams/team3/util.go
@@ -5,7 +5,7 @@ import (
 	"math/rand"
 )
 
-// Update Utility
+// Update Utility 
 func (a *AgentThree) UpdateUtility(baseAgent agent.BaseAgent) {
 	view := baseAgent.View()
 	agentState := view.AgentState()
@@ -23,4 +23,94 @@ func (a *AgentThree) UpdateUtility(baseAgent agent.BaseAgent) {
 	}
 	// Sort utility map
 	// sort.Sort(a.utilityScore)
+}
+
+
+//implemented the 3 functions resource, proposal, chair: considering the utility is between 0 to 15
+func (a *AgentThree) UtilityResource(baseAgent agent.BaseAgent){
+	//should initiate a utilityResource to 7 (score= map[id]=utilityResource)
+    //punishment:
+	for id, score := range agentscoreResource{
+		if score.id reject trade{
+			if score>=12{
+				score:= score + 1
+			}
+			else if score<=12 && score>=7{
+				score := score - 2
+			}
+			else{
+				score:= score + 3
+			}
+		}
+	}
+	//Update scoreresource map with agent's new scores
+
+    //Rewarding the Agents
+        if agent.id agrees trade // the trade has been made
+            score+=3
+
+    //Implement Boundaries
+    if score > 15{
+        score :=15
+	}
+    if score < 0{
+        score :=0
+	}
+}
+
+func (a *AgentThree) UtilityProposal(baseAgent agent.BaseAgent){
+		//should initiate a utilityProposal to 7 (score= map[id]=utilityResource)
+
+    var Lp=0
+
+    if agent has a proposal AND asked us about it AND proposal /= our decision{ //message boolean
+        Lp += 1
+	}
+    if Lp > 4{
+        score := score + 1
+        Lp := 0
+	}
+    if agent proposal AND asked us about it AND proposal = our decision{ //message boolean
+        score:= score + 1
+	}
+	If score > 15{
+        score :=15
+	}
+    if score < 0{
+        score :=0
+	}
+}
+
+        
+func (a *AgentThree)   UtilityChair (baseAgent agent.BaseAgent){
+		//should initiate a utilityChair to 7 (score= map[id]=utilityResource)
+
+	var Lc:=0
+    if fight decision =/ proposal.our fight decision{ //message changed to bool
+        Lc := Lc + 1
+	}
+    if Lc > 4:
+        score:= score 1 
+		Lc = 0
+    if ourfight == prop.ourfightdecision:
+        score = score + 1
+
+	If score > 15{
+		score :=15
+	}
+	if score < 0{
+		score :=0
+	}
+}
+
+func (a *AgentThree)   TotalUtility (baseAgent agent.BaseAgent){  //average of the 3 score
+	    utilityscore:= (UtilityChair() + UtilityProposal() + UtilityResource()) / 3
+	}
+
+//trusted social network, after each level
+TSN := make(map[int]int) 
+for id, util := range utilitymap {
+	if util>=10{
+		TSN[id]=util
+	}
 }
