a6017d2b66e2309b5dc43673233d1ad8d6e608f9
Razvan Rusu <razvan.rusu19@imperial.ac.uk>
2022-12-14 11:56:54 +0000

    Add Getters to Rule fields

--- pkg/infra/game/message/proposal/rule.go
+++ pkg/infra/game/message/proposal/rule.go
@@ -7,6 +7,14 @@ type Rule[A decision.ProposalAction] struct {
 	condition Condition
 }
 
+func (r Rule[A]) Action() A {
+	return r.action
+}
+
+func (r Rule[A]) Condition() Condition {
+	return r.condition
+}
+
 func NewRule[A decision.ProposalAction](action A, condition Condition) *Rule[A] {
 	return &Rule[A]{action: action, condition: condition}
 }
