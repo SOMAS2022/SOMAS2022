653cac77189437726e9f114a6bf7beb4aefbbf85
Wenlin Yi <2074270068@qq.com>
2022-12-15 20:39:18 +0000

    merge agent5.go

--- pkg/infra/main.go
+++ pkg/infra/main.go
@@ -27,7 +27,7 @@ import (
 var InitAgentMap = map[commons.ID]func() agent.Strategy{
 	"RANDOM": example.NewRandomAgent,
 	// "Agent5": team5.NewAgent5,
-	"TEAM5": team5.NewFivAgent,
+	"TEAM5": team5.NewAgent5,
 	"TEAM1": team1.NewSocialAgent,
 }
 
