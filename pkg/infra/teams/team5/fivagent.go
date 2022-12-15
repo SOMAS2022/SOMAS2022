5db0e5047dc445d3f46137e5c9432a42c2369bba
Tiger <lyh645@gmail.com>
2022-12-15 14:54:37 +0000

    Update to match new interface

--- pkg/infra/teams/team5/fivagent.go
+++ pkg/infra/teams/team5/fivagent.go
@@ -23,8 +23,8 @@ type FivAgent struct {
 	ttable      *TrustTable
 }
 
-func (fiv *FivAgent) FightResolution(agent agent.BaseAgent, _ commons.ImmutableList[proposal.Rule[decision.FightAction]]) immutable.Map[commons.ID, decision.FightAction] {
-	view := agent.View()
+func (fiv *FivAgent) FightResolution(baseAgent agent.BaseAgent, prop commons.ImmutableList[proposal.Rule[decision.FightAction]], proposedActions immutable.Map[commons.ID, decision.FightAction]) immutable.Map[commons.ID, decision.FightAction] {
+	view := baseAgent.View()
 	builder := immutable.NewMapBuilder[commons.ID, decision.FightAction](nil)
 	for _, id := range commons.ImmutableMapKeys(view.AgentState()) {
 		var fightAction decision.FightAction
@@ -139,7 +139,7 @@ func (fiv *FivAgent) HandleLootProposalRequest(_ message.Proposal[decision.LootA
 	}
 }
 
-func (fiv *FivAgent) LootAllocation(baseAgent agent.BaseAgent, proposal message.Proposal[decision.LootAction]) immutable.Map[commons.ID, immutable.SortedMap[commons.ItemID, struct{}]] {
+func (fiv *FivAgent) LootAllocation(baseAgent agent.BaseAgent, proposal message.Proposal[decision.LootAction], proposedAllocations immutable.Map[commons.ID, immutable.SortedMap[commons.ItemID, struct{}]]) immutable.Map[commons.ID, immutable.SortedMap[commons.ItemID, struct{}]] {
 	lootAllocation := make(map[commons.ID][]commons.ItemID)
 	view := baseAgent.View()
 	ids := commons.ImmutableMapKeys(view.AgentState())
