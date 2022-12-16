e62b53beb8c48192664f4b31ce54c4a064f3d716
jl21518 <jl21518@ic.ac.uk>
2022-12-16 14:25:11 +0000

    add fight prediction map

--- pkg/infra/teams/team5/tradeStrat.go
+++ pkg/infra/teams/team5/tradeStrat.go
@@ -9,22 +9,23 @@ import (
 )
 
 func HandleTradeNegotiation(a agent.BaseAgent, T_TradeInfo message.TradeInfo, round uint) message.TradeMessage {
-
-	if len(T_TradeInfo.Negotiations) == 0 && round < 2 && leaderfightpredictionmap[a.id]!= Cower{ //need change, no offers then request
-		return T_request()
+	leaderfightpredictionmap := FindBestStrategy(a.view)
+	T_fightDecision := leaderfightpredictionmap[a.id]
+	if len(T_TradeInfo.Negotiations) == 0 && round < 2 && T_fightDecision!= Cower{ //no offers then request
+		return T_request(a, T_fightDecision)
 	}else if round < 2{ //process requests 
-		return firstResponse(a, T_TradeInfo.Negotiations)
+		return firstResponse(a, T_TradeInfo.Negotiations, T_fightDecision)
 	}else if len(T_TradeInfo.Negotiations) != 0{ //process requests
-		return secondResponse(a, T_TradeInfo.Negotiations)
+		return secondResponse(a, T_TradeInfo.Negotiations, T_fightDecision)
 	}else{ //no offers then abstain
 		return message.TradeAbstain{}
 	}
 }
 
-func T_request(a agent.BaseAgent) message.TradeRequest{
-	T_id_attack := getTradePartnerAttack(a)
-	T_id_defend := getTradePartnerDefend(a)
-	if(leaderfightpredictionmap[id] == decision.Fight){
+func T_request(a agent.BaseAgent, T_fightDecision decision.FightAction) message.TradeRequest{
+	T_id_attack := getTradePartnerAttack(a, T_fightDecision)
+	T_id_defend := getTradePartnerDefend(a, T_fightDecision)
+	if(T_fightDecision == decision.Fight){
 		makeOffer := message.NewTradeOffer(Weapon, a.latestState.Weapons[0], weapon immutable.List[state.Item], shield immutable.List[state.Item])
 		makeDemand := message.NewTradeDemand(Weapon, minValue uint)
 	}else {
@@ -39,31 +40,30 @@ func T_request(a agent.BaseAgent) message.TradeRequest{
 		return fiveRequest
 }
 
-func getTradePartnerAttack(a agent.BaseAgent) commons.ID{
+func getTradePartnerAttack(a agent.BaseAgent, T_fightDecision decision.FightAction) commons.ID{
 	for id, T_HiddenAgentState := range a.view.agentState{
-		if leaderfightpredictionmap[id] == decision.Cower && T_HiddenAgentState.BonusAttack > a.latestState.Weapons[0]{
+		if T_fightDecision == decision.Cower && T_HiddenAgentState.BonusAttack > a.latestState.Weapons[0]{
 			return id
 		}
 	}
 	return ""
 }
-func getTradePartnerDefend(a agent.BaseAgent) commons.ID{
+func getTradePartnerDefend(a agent.BaseAgent, T_fightDecision decision.FightAction) commons.ID{
 	for id, T_HiddenAgentState := range a.view.agentState{
-		if leaderfightpredictionmap[id] == decision.Cower && T_HiddenAgentState.BonusDefense > a.latestState.Shields[0]{
+		if T_fightDecision == decision.Cower && T_HiddenAgentState.BonusDefense > a.latestState.Shields[0]{
 			return id
 		}
 	}
 	return ""
 }
 //if fighting ask for stuff, if cowering process a proposal to accept
-func firstResponse(a agent.BaseAgent, T_Negotiations TradeInfo.Negotiations) message.TradeMessage{
-	T_fightmode := leaderfightpredictionmap[a.Id] //need change
-	if T_fightmode != decision.Cower{ //need change
-		return T_request(a)
+func firstResponse(a agent.BaseAgent, T_Negotiations TradeInfo.Negotiations, T_fightDecision decision.FightAction) message.TradeMessage{
+	if T_fightDecision != decision.Cower{ 
+		return T_request(a, T_fightDecision)
 	}
 	//if i'm cowering accept first offer from attack/defend guy
 	for id,T_TradeNegotiation := range T_Negotiations{ // go through request buffer
-		if T_TradeNegotiation.RoundNum == 1 &&  leaderfightpredictionmap[id] != decision.Cower{//need change
+		if T_TradeNegotiation.RoundNum == 1 &&  T_fightDecision != decision.Cower{
 			return message.TradeAccept{
 				TradeID:	T_TradeNegotiation.Id}
 		}
@@ -71,11 +71,11 @@ func firstResponse(a agent.BaseAgent, T_Negotiations TradeInfo.Negotiations) mes
 	return message.TradeAbstain{}
 }
 
-func secondResponse(a agent.BaseAgent, T_Negotiations TradeInfo.Negotiations) message.TradeMessage{
+func secondResponse(a agent.BaseAgent, T_Negotiations TradeInfo.Negotiations, T_fightDecision decision.FightAction) message.TradeMessage{
 	T_fightmode := decision.FightAction //need change
 	//if i'm cowering accept first offer from attack/defend guy
 	for id,T_TradeNegotiation := range T_Negotiations{ // go through request buffer
-		if T_TradeNegotiation.RoundNum == 1 &&  leaderfightpredictionmap[id] != decision.Cower{//need change
+		if T_TradeNegotiation.RoundNum == 1 &&  T_fightDecision != decision.Cower{//need change
 			return message.TradeAccept{
 				TradeID:	T_TradeNegotiation.Id}
 		}
